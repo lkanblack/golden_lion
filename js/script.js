7774e5d95b125a25e7364dbb095ee7b6b3d4bb19
Andrei Dubinko <andreidubinko@Andreis-MacBook-Pro.local>
2021-11-30 18:19:31 +0200

    desk + login popup

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
 let sign_btn = document.querySelector('.sign-up');
 let sign_popup = document.querySelector('.sign-popup');
 let sign_close = document.querySelector('.close-sign');
-
-console.log(sign_btn);
+let login_btn = document.querySelector('.login');
+let login_popup = document.querySelector('.login-popup');
+let login_close = document.querySelector('.close-login');
 
 sign_btn.addEventListener('click', function() {
     sign_popup.classList.toggle('active-block');
 })
 sign_close.addEventListener('click', function(){
     sign_popup.classList.toggle('active-block');
+})
+login_btn.addEventListener('click', function(){
+    login_popup.classList.toggle('active-flex');
+})
+login_close.addEventListener('click', function(){
+    login_popup.classList.toggle('active-flex');
 })
