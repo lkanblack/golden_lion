ad9c0aa983911befb7b2c9b288220bbe97757108
Andrei Dubinko <andreidubinko@Andreis-MacBook-Pro.local>
2021-11-30 21:38:43 +0200

    mobile

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ let sign_close = document.querySelector('.close-sign');
 let login_btn = document.querySelector('.login');
 let login_popup = document.querySelector('.login-popup');
 let login_close = document.querySelector('.close-login');
+let burger_btn = document.querySelector('.burger-menu');
+let burger_list = document.querySelector('.burger-list');
 
 sign_btn.addEventListener('click', function() {
     sign_popup.classList.toggle('active-block');
@@ -16,4 +18,21 @@ login_btn.addEventListener('click', function(){
 })
 login_close.addEventListener('click', function(){
     login_popup.classList.toggle('active-flex');
+})
+
+
+burger_btn.addEventListener('click', function(){
+    burger_list.classList.toggle('active-flex');
+    let links = burger_list.querySelectorAll('a');
+    let btns = burger_list.querySelectorAll('button');
+    for(let link of links){
+        link.addEventListener('click', function(){
+            burger_list.classList.remove('active-flex');
+        })
+    }
+    for(let btn of btns){
+        btn.addEventListener('click', function(){
+            burger_list.classList.remove('active-flex');
+        })
+    }
 })
